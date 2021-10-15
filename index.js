36e5d79dfc6ca4d711ce4c0d35f0b68ad637bd9c
JohnMig <jjuanndelacruz@gmail.com>
2021-10-15 19:29:27 +0800

    updates

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 app.disable('server')
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => res.send('Node.js Application'))
+app.get('/', (req, res) => {
+  res.removeHeader('server')
+  res.send('Node.js Application')
+})
 
 app.listen(process.env.PORT || 5000)
